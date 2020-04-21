2e2774934f9b9df066162db2dfedbde0a8085915
Gaurav Thapa <60711974+GauravThapa54@users.noreply.github.com>
2020-04-20 23:54:44 -0700

    Add files via upload
    Creating Loop

---
+++ Loop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int i = 1; i < 20; i++)
+            {
+
+                if (i % 3 == 0)  // the output keeps changing through out the loop
+                {
+                    sum = sum + i;
+                }
+            }
+            Console.WriteLine($"The sum is {sum}");
+        }
+       
+    }
+}
