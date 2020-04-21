3332cd60957b76c0f5f4fd8be0636dc66f7d7bbe
Gaurav Thapa <60711974+GauravThapa54@users.noreply.github.com>
2020-04-20 23:53:40 -0700

    Add files via upload
    Handling Arrays

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Arrays.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new List<string>{ "<name of friends>", "Ana", "felipe" };
+
+            names.Add("Johnny");
+            names.Add("Phil");
+            names.Add("Jenny");
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+            Console.WriteLine(names[0]);
+
+            //The other method of Array 
+
+
+            //var names = new List<string>{ "<name>", "Ana", "felipe" };
+            //foreach (string name in names)
+            //{
+            //    Console.WriteLine($"Hello {name.ToUpper()}!");
+            //}
+            //for ( int i = 0; i < names.Count; i ++)
+            //{
+            //    Console.WriteLine($"hello {names[i].ToUpper()}!"); ;
+            //}
+
+        }
+    }
+}
