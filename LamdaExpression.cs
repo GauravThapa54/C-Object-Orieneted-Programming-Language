7be109541fa4c347a283f8bad676b4527c0a6e1d
Gaurav Thapa <60711974+GauravThapa54@users.noreply.github.com>
2020-04-28 23:24:37 -0700

    Add files via upload
    bringing Square output values

---
+++ LamdaExpression.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda_Expression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 2, 3, 4, 5, 6, 10 };
+            var squaredNumbers = numbers.Select(x => x * x);
+            Console.WriteLine(string.Join(" ", squaredNumbers));
+        }
+    }
+}
