0b7b46c4e07a37bf7fe47370c5f5716824050d2a
Gaurav Thapa <60711974+GauravThapa54@users.noreply.github.com>
2020-04-28 23:22:37 -0700

    Add files via upload

---
+++ LambdaExpression_Part2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaExpression_Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> myList = new List<Student>();
+
+            myList.Add(new Student("Mystique", "Human Anatomy", "3.2"));
+            myList.Add(new Student("Charles", "Phsyschology", "4.0"));
+            myList.Add(new Student("Logan", "Biology", "2.5"));
+
+            myList.Sort((x, y) => { return x.Major.CompareTo(y.Major); });
+
+            foreach (Student st in myList)
+                Console.WriteLine(st.Name + " : "+st.Major + " Scoring a Total of " + st.GPAScore);
+        }
+
+        class Student
+        {
+            public string Name { get; set; }
+            public string Major { get; set; }
+            public string GPAScore { get; set; }
+
+            public Student(string nm, string mj, string gpsc)
+            {
+                Name = nm;
+                Major = mj;
+                GPAScore = gpsc; 
+            }
+        }
+    }
+}
